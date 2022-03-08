d6f625d57ca06b36692ba17031643de74e3eb43a
Damas Amirul Karim <damasdev@users.noreply.github.com>
2022-03-08 03:53:20 +0000

    Apply fixes from StyleCI

--- application/tests/controllers/Welcome_test.php
+++ application/tests/controllers/Welcome_test.php
@@ -26,7 +26,7 @@ class Welcome_test extends TestCase
     public function test_APPPATH()
     {
         $actual = realpath(APPPATH);
-        $expected = realpath(__DIR__ . '/../..');
+        $expected = realpath(__DIR__.'/../..');
         $this->assertEquals(
             $expected,
             $actual,
